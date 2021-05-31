1ec264389658d9f38e3148a0ee2cc3078fb4edb3
Rory Middleton <rorymiddleton@kubrickgroup.com>
2021-05-31 11:00:35 +0000

    Update Initial analysis.py, include linear regression

--- Final working documents, may need testing/Initial analysis.py
+++ Final working documents, may need testing/Initial analysis.py
@@ -2,22 +2,40 @@ import numpy as np
 import pandas as pd
 import seaborn as sns
 from matplotlib import pyplot as plt
+import warnings
+warnings.filterwarnings('ignore')
+import statsmodels.api as sm
        # need csv file in same folder as this module :)
 df = pd.read_csv('sentiments_returns.csv')
 for i in [1,2,3,5,10]:
-        df[f'{str(i)}daily_return_over_market'] = df[f'{i}daily_return'] - df[f'{i}daily_return_market']
+        df[f'daily_return_over_market_{str(i)}'] = df[f'{i}daily_return'] - df[f'{i}daily_return_market']
 for i in [1,2,3,5,10]:
-        df[f'{str(i)}daily_volatility_over_market'] = abs(df[f'{i}daily_return']) / abs(df[f'{i}daily_return_market'])
+        df[f'daily_volatility_over_market_{str(i)}'] = abs(df[f'{i}daily_return']) / abs(df[f'{i}daily_return_market'])
 df['positivity'] = df['Positive'] / df['Negative']
+
+model_lin = sm.OLS.from_formula(formula = "daily_return_over_market_1 ~ Positive + Negative + Uncertainty + Litigious + Constraining + Superfluous + Interesting + Modal", data=df)
+result_lin = model_lin.fit()
+print(result_lin.summary())
+'''
+Not statistically significant findings here for returns
+'''
+
+model_lin = sm.OLS.from_formula(formula = "daily_volatility_over_market_1 ~ Positive + Negative + Uncertainty + Litigious + Constraining + Superfluous + Interesting + Modal", data=df)
+result_lin = model_lin.fit()
+print(result_lin.summary())
+'''
+Statistically significant affect on volatility when high count of Litigous or Interesting words. Dive deeper into this.
+'''
+
 for i in [1,2,3,5,10]:
-        sns.lmplot(x='positivity',y=f'{i}daily_return_over_market',data = df, fit_reg = True)
+        sns.lmplot(x='positivity',y=f'daily_return_over_market_{i}',data = df, fit_reg = True)
         plt.xlim(0,1.5)
         plt.axhline(0)
         plt.show()
 
 plt.clf()
 for i in [1,2,3,5,10]:
-        sns.lmplot(x='positivity',y=f'{i}daily_volatility_over_market',data = df, fit_reg = True)
+        sns.lmplot(x='positivity',y=f'daily_volatility_over_market_{i}',data = df, fit_reg = True)
         plt.xlim(0,1.5)
         plt.axhline(1)
         plt.show()

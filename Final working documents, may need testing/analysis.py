572432dee5808a6ae5d88ebf90e34c1f872c08f9
Alfie Crust <alfiecrust@kubrickgroup.com>
2021-06-01 11:31:01 +0100

    saves summary data to excel

--- Final working documents, may need testing/analysis.py
+++ Final working documents, may need testing/analysis.py
@@ -6,7 +6,7 @@ import warnings
 warnings.filterwarnings('ignore')
 import statsmodels.api as sm
        # need csv file in same folder as this module :)
-
+       
 
 def run_analysis():
     df = pd.read_csv('sentiments_returns.csv')
@@ -18,13 +18,25 @@ def run_analysis():
     
     model_lin = sm.OLS.from_formula(formula = "daily_return_over_market_1 ~ Positive + Negative + Uncertainty + Litigious + Constraining + Superfluous + Interesting + Modal", data=df)
     result_lin = model_lin.fit()
+    
+    text = result_lin.summary().as_csv()
+    f = open('returns.csv', 'w')
+    f.write(text)
+    f.close()    
+    
     print(result_lin.summary())
+    
     '''
     Not statistically significant findings here for returns
     '''
     
     model_lin = sm.OLS.from_formula(formula = "daily_volatility_over_market_1 ~ Positive + Negative + Uncertainty + Litigious + Constraining + Superfluous + Interesting + Modal", data=df)
     result_lin = model_lin.fit()
+    
+    text = result_lin.summary().as_csv()
+    f = open('volatility.csv', 'w')
+    f.write(text)
+    f.close()    
     print(result_lin.summary())
     '''
     Statistically significant affect on volatility when high count of Litigous or Interesting words. Dive deeper into this.
@@ -34,17 +46,21 @@ def run_analysis():
             sns.lmplot(x='positivity',y=f'daily_return_over_market_{i}',data = df, fit_reg = True)
             plt.xlim(0,1.5)
             plt.axhline(0)
-            plt.show()
+            #plt.show()
     
     plt.clf()
     for i in [1,2,3,5,10]:
             sns.lmplot(x='positivity',y=f'daily_volatility_over_market_{i}',data = df, fit_reg = True)
             plt.xlim(0,1.5)
             plt.axhline(1)
-            plt.show()
+            #plt.show()
+            
+    
     '''
     No noticeable difference between positive vs negative.
     Lots of volatility in day 1 after a 10K release - should look into this
     Need to back up with hard numbers.  Look at time of year?
     '''
+    
+run_analysis()
     
